170921707f4a3ce3fe435b74d39494cfa378e866
Rodrigo Calderon <rodrigocalderon273@gmail.com>
2025-06-04 16:55:07 +0200

    Test

--- tests/test_greapy.py
+++ tests/test_greapy.py
@@ -2,23 +2,23 @@
 
 """Tests for `greapy` package."""
 
-import pytest
+# import pytest
 
 
-from greapy import greapy
+# from greapy import greapy
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+# @pytest.fixture
+# def response():
+#     """Sample pytest fixture.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#     See more at: http://doc.pytest.org/en/latest/fixture.html
+#     """
+#     # import requests
+#     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+# def test_content(response):
+#     """Sample pytest test function with the pytest fixture as an argument."""
+#     # from bs4 import BeautifulSoup
+#     # assert 'GitHub' in BeautifulSoup(response.content).title.string
